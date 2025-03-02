53db94591a1cf13d6557052adbcf760acab9494e
LRaziel <lucas.math.marreiros@gmail.com>
2025-03-02 11:43:48 -0400

    feat/adding and moving files
    Adding Dockerfile for backend +
    Adding main.py for backend +
    Adding db_models.py for backend +
    Deleting models.py +
    Moving requirements.txt to backend/requirements.txt +
    Modifying docker-compose.yml

---
+++ backend/cmd/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from .database import engine, Base
+
+app = FastAPI()
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def home():
+    return {"message": "API do sistema CME está rodando!"}
---
+++ backend/internal/models/db_models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, TIMESTAMP
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False)
+
+class Material(Base):
+    __tablename__ = "materials"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+    expiration_date = Column(Date, nullable=False)
+    serial = Column(String, unique=True, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False)
+
+class Tracking(Base):
+    __tablename__ = "tracking"
+
+    id = Column(Integer, primary_key=True, index=True)
+    material_id = Column(Integer, ForeignKey("materials.id"))
+    stage = Column(String, nullable=False)
+    status = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False)
+
+class Failure(Base):
+    __tablename__ = "failures"
+
+    id = Column(Integer, primary_key=True, index=True)
+    tracking_id = Column(Integer, ForeignKey("tracking.id"))
+    reason = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False)
