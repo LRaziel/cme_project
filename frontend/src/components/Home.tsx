dc82780a646ead347c8b72763c140638554dcc55
LRaziel <lucas.math.marreiros@gmail.com>
2025-03-04 16:21:38 -0400

    fix/adjusting layout on home component

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -31,24 +31,20 @@ const Home: React.FC = () => {
     gridRowStart: 2,
   };
 
-  const formTracking: React.CSSProperties = {
-    margin: '2%',
-  }
-
   return (
     <div style={mainsGrid}>
       <div style={title}><h1>TESTE - SISTEMA CME</h1></div>
       <div style={content}>
         <div className="container">
           <div className="row justify-content-center">
-            <div className="accordion col-6" id="groupFormUSer">
+            <div className="accordion col-6" id="groupFormUser">
               <div className="accordion-item">
                 <h2 className="accordion-header" id="headingOne">
-                  <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                     Cadastro de Usuários
                   </button>
                 </h2>
-                <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                   <div className="accordion-body">
                     <FormUser />
                   </div>
@@ -71,11 +67,22 @@ const Home: React.FC = () => {
               </div>
             </div>
 
-            <div className="col" style={formTracking}>
-              <FormTracking />
+            <div className="accordion col-12" id="groupFormTracking" style={{ marginTop: '2%' }}>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="headingThree">
+                  <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Cadastro da Rastreabilidade
+                  </button>
+                </h2>
+                <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                  <div className="accordion-body">
+                    <FormTracking />
+                  </div>
+                </div>
+              </div>
             </div>
 
-            <div>
+            <div className="col-12" style={{ marginTop: '2%' }}>
               <TrackingTable />
             </div>
           </div>
